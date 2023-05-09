617de5be31967f83b01e4c3756694154b09dc3b4
Döring Christian <christian.doering@um.de>
2023-05-09 11:15:18 +0200

    initial

---
+++ nr.py
@@ -0,0 +1,222 @@
+import mitsuba as mi
+import drjit as dr
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+import matplotlib.pyplot as plt
+
+mi.set_variant("cuda_ad_rgb")
+
+
+def mis_weight(pdf_a: mi.Float, pdf_b: mi.Float) -> mi.Float:
+    """
+    Compute the Multiple Importance Sampling (MIS) weight given the densities
+    of two sampling strategies according to the power heuristic.
+    """
+    a2 = dr.sqr(pdf_a)
+    return dr.detach(dr.select(pdf_a > 0, a2 / dr.fma(pdf_b, pdf_b, a2), 0), True)
+
+
+class PathIntegrator(mi.SamplingIntegrator):
+    def __init__(self, props: mi.Properties):
+        super().__init__(props)
+        self.max_depth: int = props.get("max_depth", 8)
+        self.rr_depth: int = props.get("rr_depth", 2)
+        self.n = 0
+        self.film_size: None | mi.Vector2u = None
+
+    def render(
+        self,
+        scene: mi.Scene,
+        sensor: mi.Sensor,
+        seed: int = 0,
+        spp: int = 1,
+        develop: bool = True,
+        evaluate: bool = True,
+    ):
+        film = sensor.film()
+
+        film_size = film.crop_size()
+
+        if self.film_size is None:
+            self.film_size = film_size
+
+        wavefront_size = film_size.x * film_size.y * spp
+        print(f"{wavefront_size=}")
+
+        sampler = sensor.sampler()
+        sampler.set_sample_count(spp)
+        sampler.set_samples_per_wavefront(spp)
+        sampler.seed(seed, wavefront_size)
+
+        idx = dr.arange(mi.UInt, wavefront_size)
+
+        pos = mi.Vector2u()
+        pos.y = idx // film_size.x
+        pos.x = dr.fma(-film_size.x, pos.y, idx)
+
+        sample_pos = (mi.Point2f(pos) + sampler.next_2d()) / mi.Point2f(
+            film.crop_size()
+        )
+        ray, ray_weight = sensor.sample_ray(0.0, 0.0, sample_pos, mi.Point2f(0.5))
+
+        spec = ray_weight * self.sample(scene, sampler, ray)
+
+        film.prepare(self.aov_names())
+
+        block: mi.ImageBlock = film.create_block()
+
+        aovs = [spec.x, spec.y, spec.z, mi.Float(1.0)]
+
+        block.put(pos, aovs)
+
+        film.put_block(block)
+
+        res = film.develop()
+        dr.schedule(res)
+        dr.eval()
+
+        return res
+
+    def sample(
+        self,
+        scene: mi.Scene,
+        sampler: mi.Sampler,
+        ray: mi.Ray3f,
+        active: bool = True,
+    ) -> mi.Color3f:
+        # --------------------- Configure loop state ----------------------
+
+        ray = mi.Ray3f(ray)
+        active = mi.Bool(active)
+        throughput = mi.Spectrum(1.0)
+        result = mi.Spectrum(0.0)
+        eta = mi.Float(1.0)
+        depth = mi.UInt32(0)
+
+        valid_ray = mi.Bool(scene.environment() is not None)
+
+        # Variables caching information from the previous bounce
+        prev_si: mi.SurfaceInteraction3f = dr.zeros(mi.SurfaceInteraction3f)
+        prev_bsdf_pdf = mi.Float(1.0)
+        prev_bsdf_delta = mi.Bool(True)
+        bsdf_ctx = mi.BSDFContext()
+
+        loop = mi.Loop(
+            "Path Tracer",
+            state=lambda: (
+                sampler,
+                ray,
+                throughput,
+                result,
+                eta,
+                depth,
+                valid_ray,
+                prev_si,
+                prev_bsdf_pdf,
+                prev_bsdf_delta,
+                active,
+            ),
+        )
+
+        loop.set_max_iterations(self.max_depth)
+
+        while loop(active):
+            si = scene.ray_intersect(ray)  # TODO: not necesarry in first interaction
+
+            # ---------------------- Direct emission ----------------------
+
+            ds = mi.DirectionSample3f(scene, si, prev_si)
+            em_pdf = mi.Float(0.0)
+
+            em_pdf = scene.pdf_emitter_direction(prev_si, ds, ~prev_bsdf_delta)
+
+            mis_bsdf = mis_weight(prev_bsdf_pdf, em_pdf)
+
+            result = dr.fma(
+                throughput,
+                ds.emitter.eval(si, prev_bsdf_pdf > 0.0) * mis_bsdf,
+                result,
+            )
+
+            active_next = ((depth + 1) < self.max_depth) & si.is_valid()
+
+            bsdf: mi.BSDF = si.bsdf(ray)
+
+            # ---------------------- Emitter sampling ----------------------
+
+            active_em = active_next & mi.has_flag(bsdf.flags(), mi.BSDFFlags.Smooth)
+
+            ds, em_weight = scene.sample_emitter_direction(
+                si, sampler.next_2d(), True, active_em
+            )
+
+            wo = si.to_local(ds.d)
+
+            # ------ Evaluate BSDF * cos(theta) and sample direction -------
+
+            sample1 = sampler.next_1d()
+            sample2 = sampler.next_2d()
+
+            bsdf_val, bsdf_pdf, bsdf_sample, bsdf_weight = bsdf.eval_pdf_sample(
+                bsdf_ctx, si, wo, sample1, sample2
+            )
+
+            # --------------- Emitter sampling contribution ----------------
+
+            bsdf_val = si.to_world_mueller(bsdf_val, -wo, si.wi)
+
+            mi_em = dr.select(ds.delta, 1.0, mis_weight(ds.pdf, bsdf_pdf))
+
+            result[active_em] = dr.fma(throughput, bsdf_val * em_weight * mi_em, result)
+
+            # ---------------------- BSDF sampling ----------------------
+
+            bsdf_weight = si.to_world_mueller(bsdf_weight, -bsdf_sample.wo, si.wi)
+
+            ray = si.spawn_ray(si.to_world(bsdf_sample.wo))
+
+            # ------ Update loop variables based on current interaction ------
+
+            throughput *= bsdf_weight
+            eta *= bsdf_sample.eta
+            valid_ray |= (
+                active
+                & si.is_valid()
+                & ~mi.has_flag(bsdf_sample.sampled_type, mi.BSDFFlags.Null)
+            )
+
+            prev_si = si
+            prev_bsdf_pdf = bsdf_sample.pdf
+            prev_bsdf_delta = mi.has_flag(bsdf_sample.sampled_type, mi.BSDFFlags.Delta)
+
+            # -------------------- Stopping criterion ---------------------
+
+            depth[si.is_valid()] += 1
+
+            throughput_max = dr.max(throughput)
+
+            rr_prop = dr.minimum(throughput_max * dr.sqr(eta), 0.95)
+            rr_active = depth >= self.rr_depth
+            rr_continue = sampler.next_1d() < rr_prop
+
+            throughput[rr_active] *= dr.rcp(rr_prop)
+
+            active = (
+                active_next & (~rr_active | rr_continue) & (dr.neq(throughput_max, 0.0))
+            )
+
+        return dr.select(valid_ray, result, 0.0)
+
+
+mi.register_integrator("path_test", lambda props: PathIntegrator(props))
+
+if __name__ == "__main__":
+    with dr.suspend_grad():
+        scene: mi.Scene = mi.load_dict(mi.cornell_box())
+
+        integrator: mi.Integrator = mi.load_dict({"type": "path_test"})
+
+        img = integrator.render(scene, scene.sensors()[0], 0)
+
+        plt.imshow(img)
+        plt.show()
