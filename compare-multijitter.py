9ae3aa147bff3704d80207cb423e8be4e6ca80de
Döring Christian <christian.doering@um.de>
2023-05-27 11:02:19 +0200

    test multijitter

--- compare-multijitter.py
+++ compare-multijitter.py
@@ -9,38 +9,40 @@ from nerad import NRField, NeradIntegrator
 if __name__ == "__main__":
     scene_dict = mi.cornell_box()
 
-    scene_dict.pop("small-box")
-    scene_dict.pop("large-box")
+    # scene_dict.pop("small-box")
+    # scene_dict.pop("large-box")
     # scene_dict["sphere"] = {
     #     "type": "sphere",
     #     "to_world": mi.ScalarTransform4f.translate([0.335, 0.0, -0.38]).scale(0.5),
-    #     "bsdf": {"type": "plastic"},
+    #     "bsdf": {"type": "dielectric"},
     # }
 
-    scene_dict["suzanne"] = {
-        "type": "ply",
-        "filename": "./data/meshes/suzanne.ply",
-        "to_world": mi.ScalarTransform4f.translate([0.335, 0.0, 0.0])
-        .rotate([1.0, 0.0, 0.0], 90.0)
-        .scale(0.5),
-        "bsdf": {"type": "dielectric"},
-    }
+    # scene_dict["suzanne"] = {
+    #     "type": "ply",
+    #     "filename": "./data/meshes/suzanne.ply",
+    #     "to_world": mi.ScalarTransform4f.translate([0.335, 0.0, 0.0])
+    #     .rotate([1.0, 0.0, 0.0], 90.0)
+    #     .scale(0.5),
+    #     "bsdf": {"type": "dielectric"},
+    # }
 
     scene: mi.Scene = mi.load_dict(scene_dict)
 
     field = NRField(scene, n_hidden=3, width=256)
-    integrator = NeradIntegrator(field, M=16)
+    integrator = NeradIntegrator(field, M=16, total_steps=100)
     integrator.train(scene)
-    image_independent = mi.render(scene, spp=1, integrator=integrator)
+    image_independent = mi.render(scene, spp=16, integrator=integrator)
     losses_independent = integrator.train_losses
 
     l_sampler: mi.Sampler = mi.load_dict({"type": "multijitter", "sample_count": 1})
     r_sampler: mi.Sampler = mi.load_dict({"type": "multijitter", "sample_count": 1})
 
     field = NRField(scene, n_hidden=3, width=256)
-    integrator = NeradIntegrator(field, l_sampler=l_sampler, r_sampler=r_sampler, M=16)
+    integrator = NeradIntegrator(
+        field, l_sampler=l_sampler, r_sampler=r_sampler, M=16, total_steps=100
+    )
     integrator.train(scene)
-    image_multi = mi.render(scene, spp=1, integrator=integrator)
+    image_multi = mi.render(scene, spp=16, integrator=integrator)
     losses_multi = integrator.train_losses
 
     fig, ax = plt.subplots(2, 2, figsize=(10, 10))
@@ -48,11 +50,13 @@ if __name__ == "__main__":
 
     ax[0][0].axis("off")
     ax[0][0].imshow(mi.util.convert_to_bitmap(image_independent))
+    ax[0][0].set_title("independent")
     ax[1][0].plot(losses_independent)
 
     ax[0][1].axis("off")
-    ax[0][1].imshow(mi.util.convert_to_bitmap(image_independent))
-    ax[1][1].plot(losses_independent)
+    ax[0][1].set_title("multijitter")
+    ax[0][1].imshow(mi.util.convert_to_bitmap(image_multi))
+    ax[1][1].plot(losses_multi)
 
     fig.tight_layout()
     plt.show()
